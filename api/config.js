8e33c19d612d55d51c7716a0ff3003a82914bd75
poomthep <poomthep@gmail.com>
2025-08-26 18:29:40 +0700

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-module.exports = async function (req, res) {
+export default async function handler(req, res) {
   try {
     res.setHeader('Cache-Control', 'no-store');
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
