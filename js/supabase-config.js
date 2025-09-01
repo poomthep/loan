7cf647071d8d2cd096a9e44f1761a706d834335f
poomthep <poomthep@gmail.com>
2025-09-01 22:09:20 +0700

    Add files via upload

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // /js/supabase-config.js  (ESM)
-export const SUPABASE_URL = 'https://kpsferwaplnkzrbqoghv.supabase.co;
+export const SUPABASE_URL = 'https://kpsferwaplnkzrbqoghv.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwc2ZlcndhcGxua3pyYnFvZ2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTI1NjUsImV4cCI6MjA3MTA4ODU2NX0.FizC7Ia92dqvbtfuU5T3hymh-UX6OEqQRvQnB0oY96Y';
 
 // เส้นทางหลังล็อกอิน/ล็อกเอาต์
