349f1aed6a5189efd009125d2b5711144624e5a7
poomthep <poomthep@gmail.com>
2025-09-02 16:35:02 +0700

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// ใส่ค่าโปรเจกต์ของคุณที่นี่ แล้วเซฟไฟล์
-// สำคัญ: ห้าม commit ANON KEY ในที่สาธารณะ
-window.SUPABASE_CONFIG = {
-  url: "https://kpsferwaplnkzrbqoghv.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwc2ZlcndhcGxua3pyYnFvZ2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTI1NjUsImV4cCI6MjA3MTA4ODU2NX0.FizC7Ia92dqvbtfuU5T3hymh-UX6OEqQRvQnB0oY96Y"
-};
