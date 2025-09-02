77aeef85730d6094ad46a6ed8285cc2ca51bc8c5
poomthep <poomthep@gmail.com>
2025-09-02 20:50:39 +0700

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  SUPABASE_URL: "https://kpsferwaplnkzrbqoghv.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwc2ZlcndhcGxua3pyYnFvZ2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTI1NjUsImV4cCI6MjA3MTA4ODU2NX0.FizC7Ia92dqvbtfuU5T3hymh-UX6OEqQRvQnB0oY96Y",
-  REDIRECTS: {
-    afterLoginUser: "/loan.html",
-    afterLoginAdmin: "/admin.html",
-    afterLogout: "/"
-  }
-};
